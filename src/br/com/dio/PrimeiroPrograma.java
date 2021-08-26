084a207fc75996804f8b6d740504987f5c114689
Matheus Romano Braga <romanobraga.m@gmail.com>
2021-08-26 09:49:52 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+    }
+}
